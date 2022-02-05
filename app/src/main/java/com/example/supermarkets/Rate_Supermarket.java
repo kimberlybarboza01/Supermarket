e7cd51d781c612cbeb5eb52dad19795e1c711a66
kimberlybarboza01 <95250937+kimberlybarboza01@users.noreply.github.com>
2022-02-04 19:33:48 -0500

    superMarket rating complete

--- app/src/main/java/com/example/supermarkets/Rate_Supermarket.java
+++ app/src/main/java/com/example/supermarkets/Rate_Supermarket.java
@@ -36,9 +36,23 @@ public class Rate_Supermarket extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 RatingBar rbLiquor = findViewById(R.id.ratingBarLiquor);
+                RatingBar rbProduce = findViewById(R.id.ratingBarProduce);
+                RatingBar rbMeat = findViewById(R.id.ratingBarMeat);
+                RatingBar rbCheese = findViewById(R.id.ratingBarCheese);
+                RatingBar rbEase = findViewById(R.id.ratingBarEase);
+
+                float rateLiquor = rbLiquor.getRating();
+                float rateProduce = rbProduce.getRating();
+                float rateMeat = rbMeat.getRating();
+                float rateCheese = rbCheese.getRating();
+                float rateEase = rbEase.getRating();
+
+                float total = (rateLiquor + rateProduce + rateMeat + rateCheese + rateEase) / 5;
+
+
                 TextView avgTextView = findViewById(R.id.textViewAvg);
 
-                avgTextView.setText(String.valueOf(rbLiquor.getRating()));
+                avgTextView.setText(String.valueOf(total));
             }
         });
 
